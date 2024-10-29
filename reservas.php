cd15bf42737cd2b10e5a3cbe3334c64fbc38f267
Merichrimas <22890316@ittizimin.edu.mx>
2024-10-29 11:58:44 -1100

    Update reservas.php

--- reservas.php
+++ reservas.php
@@ -1,5 +1,5 @@
 <?php 
-$mysqli = new mysqli('localhost', 'root', 'pass', 'base de datos');
+$mysqli = new mysqli('localhost', 'meri', '0921', 'base de datos');
 
 if ($mysqli->connect_error) {
     die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
